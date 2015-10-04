c5e959e7bf2a89df4792865c7f2b9ff745b22073
John A. Huebner II <Hube2@users.noreply.github.com>
2015-10-04 10:38:14 -0400

    removed unused class

--- include/_required.php
+++ include/_required.php
@@ -4,7 +4,6 @@
 	if (!defined('WPINC')) {die;}
 	
 	$converter_file_base = dirname(__FILE__);
-	require($converter_file_base.'/options-page.php');
 	require($converter_file_base.'/post-type.php');
 	require($converter_file_base.'/settings.php');
 	require($converter_file_base.'/acf-field-groups.php');
@@ -12,9 +11,5 @@
 	require($converter_file_base.'/field-converter.php');
 	require($converter_file_base.'/admin.php');
 	require($converter_file_base.'/ajax.php');
-	
-	
-	// for testing only
-	//require(dirname(dirname(__FILE__)).'/__testing/functions.php');
 	
 ?>
